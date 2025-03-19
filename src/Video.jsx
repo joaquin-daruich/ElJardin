bf3852ef0b18fb36922b7b009111ec62094a41dd
joaquin-daruich <joaquindaruichh@gmail.com>
2025-03-19 00:53:30 -0300

    El Jardin De La Lujuria

---
+++ src/Video.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Video = () => {
+  return (
+    <>
+    <img className='infierno' src="/infierno.jpg" />
+    <video className='ElVideo' controls src="/BULMA Y 18 SON FOLLADAS POR NEGROS CACHONDOS (AUDIO LATINO).mp4"></video>
+    </>
+    
+  )
+}
+
+export default Video
